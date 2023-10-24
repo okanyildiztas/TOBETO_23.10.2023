3f437276210aac83642516e38ff534111c0c3e74
okanyildiztas <okanyldztas@gmail.com>
2023-10-25 02:24:08 +0300

    Ref Keyword İle Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -4,12 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Add();
-            Add();
-            Add();
-            Add();
-            var result = Add2();
-            Console.WriteLine(result);
+            //Add();
+            //Add();
+            //Add();
+            //Add();
+            //var result = Add2();
+
+            int number1 = 20;
+            int number2 = 100;
+            var result2 = Add3(ref number1, number2);
+            Console.WriteLine(result2);
+            Console.WriteLine(number1);
             Console.ReadLine();
         }
 
@@ -18,10 +23,16 @@
             Console.WriteLine("Added!!!");
         }
 
-        static int Add2(int number1=20, int number2=30)
+        static int Add2(int number1 = 20, int number2 = 30)
         {
             var result = number1 + number2;
             return result;
+        }
+
+        static int Add3(ref int number1, int number2)
+        {
+            number1 = 30;
+            return number1 + number2;
         }
 
       }

e47ba365f56fdce569cb67c028a1061d72374465
okanyildiztas <okanyldztas@gmail.com>
2023-10-25 02:55:35 +0300

    Diziler

---
+++ Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3] { "Engin", "Derin", "Salih" };
+           
+
+            string[] students2 = new[] { "Engin", "Derin", "Salih" };
+            students2[3] = "Ahmet";
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.WriteLine();
+
+              
+               
+        }
+    }
+}

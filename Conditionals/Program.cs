2f4e6a9ffe044f778c7e4e9b7e27e773eedf1e6c
okanyildiztas <okanyldztas@gmail.com>
2023-10-24 23:33:14 +0300

    Else If Bloğuyla Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -1,11 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-var number = 10;
-if (number==10)
+var number = 11;
 {
-    Console.WriteLine("Number is 10");
+    Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
 }
-else
-{
-    Console.WriteLine("Number is not 10");
-}
-Console.ReadLine();
+
+
+    

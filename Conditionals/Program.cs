318cceaf5ffda9dff61598ff9dad14c211d17844
okanyildiztas <okanyldztas@gmail.com>
2023-10-25 00:55:52 +0300

    Switch Bloğuyla Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -9,7 +9,7 @@ if (number == 10)
 {
     Console.WriteLine("Nummber is 10");
 }
-else if(number ==20)
+else if (number == 20)
 {
     Console.WriteLine("Number is 20");
 }
@@ -17,4 +17,18 @@ else
 {
     Console.WriteLine("Number is not 10 or 20");
 }
-    
+
+
+switch (number)
+{
+    case 10:
+        Console.WriteLine("Number is 10");
+        break;
+    case 20:
+        Console.WriteLine("Number is 20");
+        break;
+    default:
+        Console.WriteLine("Number is not 10 or 20");
+        break;
+
+}

8e4177c8dbe89689cc8086ab24fb9309b2c196f6
okanyildiztas <okanyldztas@gmail.com>
2023-10-25 01:25:31 +0300

    İç İçe If Bloklarıyla Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -34,7 +34,7 @@ switch (number)
 
 }
 */
-
+/*
 if (number >= 0 && number <= 100)
 {
     Console.WriteLine("Number is between 0-100");
@@ -46,4 +46,18 @@ else if (number > 100 && number <= 200)
 else if(number>200 || number < 0)
 {
     Console.WriteLine("Number is less than 0 or greater than 200");
+}
+*/
+
+if(number<100)
+{
+    if(number>=90)
+    {
+
+    }
+    else
+    {
+
+    }
+
 }

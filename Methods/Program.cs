f3923bf6437a4953e30a6297f6c8493d3ad84d8a
okanyildiztas <okanyldztas@gmail.com>
2023-10-25 02:43:44 +0300

    Params Keyword İle Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -20,6 +20,8 @@
 
             Console.WriteLine(Multiply(2, 4));
             Console.WriteLine(Multiply(2, 4,5));
+
+            Console.WriteLine(Add4(1, 2, 3, 4, 5, 6));
             Console.ReadLine();
         }
 
@@ -49,6 +51,12 @@
         {
             return number1 * number2 * number3;
         }
+
+        static int Add4(int number,params int[] numbers)
+        {
+            return numbers.Sum();
+        }
+
 
     }
     }

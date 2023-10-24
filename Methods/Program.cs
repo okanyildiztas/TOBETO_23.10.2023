3200e65ab00a7e24e36ff0bcb71122e5b2e7ba18
okanyildiztas <okanyldztas@gmail.com>
2023-10-25 02:30:49 +0300

    Out Keyword İle Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -10,9 +10,11 @@
             //Add();
             //var result = Add2();
 
-            int number1 = 20;
+            int number1;
             int number2 = 100;
-            var result2 = Add3(ref number1, number2);
+            var result2 = Add3(out number1, number2);
+
+
             Console.WriteLine(result2);
             Console.WriteLine(number1);
             Console.ReadLine();
@@ -23,13 +25,13 @@
             Console.WriteLine("Added!!!");
         }
 
-        static int Add2(int number1 = 20, int number2 = 30)
+        static int Add2(int number1=20, int number2=30)
         {
             var result = number1 + number2;
             return result;
         }
 
-        static int Add3(ref int number1, int number2)
+        static int Add3(out int number1, int number2)
         {
             number1 = 30;
             return number1 + number2;

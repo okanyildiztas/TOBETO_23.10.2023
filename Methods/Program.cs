d5d57b78bbdecdb35f773dcb3fa81e60d727bbb8
okanyildiztas <okanyldztas@gmail.com>
2023-10-25 02:05:39 +0300

    Parametreli Metotlarla Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -7,13 +7,22 @@
             Add();
             Add();
             Add();
+            Add();
+            var result = Add2(20,30);
+            Console.WriteLine(result);
             Console.ReadLine();
         }
-        
-            static void Add() 
-            {
-                Console.WriteLine("Added!");
-            }
 
+        static void Add()
+        {
+            Console.WriteLine("Added!");
         }
+
+        static int Add2(int number1, int number2)
+        {
+            var result = number1 + number2;
+            return result;
+        }
+
+      }
     }

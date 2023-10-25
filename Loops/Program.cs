249d6d52c1e6d7fb19c084af22e83707ebb9cb05
okanyildiztas <okanyldztas@gmail.com>
2023-10-25 15:05:20 +0300

    For Döngüsü

---
+++ Loops/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 100; i >=0; i=i-2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finished!");
+            
+        }
+    }
+}

10fb4a7747a9bf1b6b501c05f9524c00f410d537
okanyildiztas <okanyldztas@gmail.com>
2023-10-25 15:14:47 +0300

    While Döngüsü

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -4,6 +4,20 @@
     {
         static void Main(string[] args)
         {
+            //ForLoop();
+            int number = 100;
+            while (number>=0)
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+            Console.WriteLine("Now number is {0}", number);
+
+        }
+        private static void ForLoop()
+        {
+
+            
             for (int i = 100; i >=0; i=i-2)
             {
                 Console.WriteLine(i);

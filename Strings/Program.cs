afa982b6ecce96b238d02c76c31a4deb1cdfcfa4
okanyildiztas <okanyldztas@gmail.com>
2023-10-26 16:58:27 +0300

    String Metodlarıyla Çalışmak

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -4,6 +4,41 @@
     {
         static void Main(string[] args)
         {
+            //Intro();
+            string sentence = "My name is Engin Demiroğ";
+
+            var result = sentence.Length;
+            var result2 = sentence.Clone();
+            sentence = "My name is Derin Demiroğ";
+
+            bool result3 = sentence.EndsWith("ğ");
+            bool result4 = sentence.StartsWith("My name");
+
+            var result5 = sentence.IndexOf("name");
+            var result6 = sentence.IndexOf(" ");
+            var result7 = sentence.LastIndexOf(" ");
+            var result8 = sentence.Insert(0, "Hello, ");
+            var result9 = sentence.Substring(3, 4);
+            var result10 = sentence.ToLower();
+            var result11 = sentence.ToUpper();
+            var result12 = sentence.Replace(" ", "-");
+            var result13 = sentence.Remove(2,5);
+
+            Console.WriteLine(result13);
+            Console.ReadLine();
+
+
+        }
+
+            
+
+
+
+
+
+
+
+            /*
             string city = "Ankara";
             //Console.WriteLine(city[0]);
 
@@ -15,7 +50,7 @@
             string city2 = "İstanbul";
             string result = city + city2;
 
-            Console.WriteLine(String.Format("{0} {1}", city, city2));  
+            Console.WriteLine(String.Format("{0} {1}", city, city2));
+            */
         }
     }
-}

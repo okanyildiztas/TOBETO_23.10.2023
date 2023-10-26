01261097f0073266795478179d5663f352436b8b
okanyildiztas <okanyldztas@gmail.com>
2023-10-26 23:17:08 +0300

    Encapsulation

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -9,7 +9,14 @@ namespace Classes
     internal class Customer
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
+
+        private string _firstName;    
+        public string FirstName
+        {
+            get { return "Mrs." + _firstName; }
+            set { _firstName = value; }
+       
+        }
         public string LastName { get; set;}
         public string City { get; set; }
 

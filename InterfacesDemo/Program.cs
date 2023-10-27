1771bd8fa313b9e95290612dc6e49521d0d426da
okanyildiztas <okanyldztas@gmail.com>
2023-10-27 14:48:48 +0300

    Çoklu İmplementasyon

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace InterfacesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers = new IWorker[3]
+            {
+                new Worker() ,
+                new Manager(),
+                new Robot() ,
+            };
+            foreach (var worker in workers)
+            {
+                worker.Work();
+            }
+            IEat[] eats = new IEat[2]
+            {
+                new Worker(),
+                new Manager(),
+            };
+            foreach (var eat in eats)
+            {
+                eat.Eat();
+            }
+        }
+    }
+
+    interface IWorker
+    {
+        void Work();
+    }
+
+    interface IEat
+    {
+        void Eat();
+    }
+
+    interface ISalary
+    {
+        void Salary();
+    }
+
+    class Manager : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("abc");
+        }
+
+        public void Salary()
+        {
+            Console.WriteLine("abc");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("abc");
+        }
+    }
+
+    class Worker : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("abc");
+        }
+
+        public void Salary()
+        {
+            Console.WriteLine("abc");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("abc");
+        }
+    }
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            Console.WriteLine("abc");
+
+        }
+    }
+}

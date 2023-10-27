3e862aa9b9e0dccfb6377b6ad94556355e359546
okanyildiztas <okanyldztas@gmail.com>
2023-10-27 12:25:09 +0300

    İnterfacelere Temel Giriş

---
+++ Interfaces/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
+    interface IPerson
+    {
+        int Id { get; set; }
+        int FirstName {  get; set; }
+        int LastName { get; set; }
+
+    }
+    
+    class Customer:IPerson
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public string Address { get; set; }
+
+    }
+
+    class Student:IPerson
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public string Departmant {  get; set; }
+    }
+}

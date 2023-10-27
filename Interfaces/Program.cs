09ed339143dab6e5cca77f96d48bf7e1247f44bf
okanyildiztas <okanyldztas@gmail.com>
2023-10-27 13:09:14 +0300

    İnterfaceleri Neden Kullanıyoruz?

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -4,23 +4,44 @@
     {
         static void Main(string[] args)
         {
-           
+            PersonManager manager = new PersonManager();
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Engin",
+                LastName = "Demiroğ",
+                Address = "Ankara"
+
+            };
+            Student student = new Student
+            {
+                Id=1,
+                FirstName = "Derin",
+                LastName = "Demiroğ",
+                Departmant = "Computer Sciences"
+            };
+            manager.Add(customer);
+            manager.Add(student);
         }
+
     }
+
+       
+    
 
     interface IPerson
     {
         int Id { get; set; }
-        int FirstName {  get; set; }
-        int LastName { get; set; }
+        string FirstName {  get; set; }
+        string LastName { get; set; }
 
     }
     
     class Customer:IPerson
     {
         public int Id { get; set; }
-        public int FirstName { get; set; }
-        public int LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Address { get; set; }
 
     }
@@ -28,8 +49,24 @@
     class Student:IPerson
     {
         public int Id { get; set; }
-        public int FirstName { get; set; }
-        public int LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Departmant {  get; set; }
+    }
+
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(IPerson person )
+        {
+            Console.WriteLine(person.FirstName);
+        }
     }
 }
